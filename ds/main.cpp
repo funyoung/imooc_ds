bcda83b27bf5d08e8985b014961abbb623bee405
f <fy@local.com>
2021-05-11 21:09:01 +0800

    Initial Commit

---
+++ ds/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ds
+//
+//  Created by yangfeng on 2021/5/11.
+//  Copyright © 2021 yangfeng. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
